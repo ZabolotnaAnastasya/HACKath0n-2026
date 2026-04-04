74bd272e5e3d72ef8f427e1d1a6f6b33c23d13b4
A.Zabolotna <zabolotna.anastasya@gmail.com>
2026-04-04 18:06:30 +0300

    speed vector fix and -g

--- backend/app/services/navigation/engine.py
+++ backend/app/services/navigation/engine.py
@@ -6,26 +6,28 @@ class NavigationEngine:
         self.position = np.array([0.0, 0.0, 0.0])
         self.velocity = np.array([0.0, 0.0, 0.0])
         self.orientation = R.from_quat([0, 0, 0, 1])
-        self.gravity_global = np.array([0.0, 0.0, 9.81])
+        # В системі ENU Up - це +Z, тому гравітація спрямована вниз (-9.81)
+        self.gravity_global = np.array([0.0, 0.0, -9.81])
         self.accel_bias_body = np.array([0.0, 0.0, 0.0])
         self.accel_prev_global = np.array([0.0, 0.0, 0.0])
 
     def predict(self, acc_raw, gyr_raw, dt):
-        if dt <= 0:
+        if dt <= 0 or dt > 0.1: # Захист від занадто великих кроків часу
             return self.position, np.linalg.norm(self.velocity)
 
         # Оновлення орієнтації
         delta_rot = R.from_rotvec(gyr_raw * dt)
         self.orientation = self.orientation * delta_rot
 
-        # Очищення від байасу (в системі координат дрона)
+        # Очищення від байасу та перетворення в глобальну систему ENU
         acc_body_clean = acc_raw - self.accel_bias_body
-
-        # Перетворення в глобальну систему ENU
         acc_global = self.orientation.apply(acc_body_clean)
 
+        # Розрахунок лінійного прискорення (віднімаємо гравітацію)
+        # Оскільки gravity_global = [0,0,-9.81], віднімання дасть +9.81 компенсації
         acc_linear = acc_global - self.gravity_global
 
+        # Метод трапецій для швидкості та позиції
         new_velocity = self.velocity + (self.accel_prev_global + acc_linear) * 0.5 * dt
         self.position += (self.velocity + new_velocity) * 0.5 * dt
 
@@ -35,6 +37,6 @@ class NavigationEngine:
         return self.position, np.linalg.norm(self.velocity)
 
     def correct(self, gps_pos, gps_vel=None, alpha=1.0):
-        self.position = gps_pos
+        self.position = (1.0 - alpha) * self.position + alpha * gps_pos
         if gps_vel is not None:
             self.velocity = (1.0 - alpha) * self.velocity + alpha * gps_vel
